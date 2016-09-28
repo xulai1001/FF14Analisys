afa3bd2fbaaf4cc312703ce7ede0813a2a4c947c
xulai1001 <xulai1001@gmail.com>
2016-09-28 15:35:32 +0800

    Sept 28 add local database for auto-complete

--- app/controller/analisysController.js
+++ app/controller/analisysController.js
@@ -1,10 +1,28 @@
-var findItemInfo = require('../protocol/findItemInfo');
 var getItemTradeData = require('../protocol/getItemTradeData');
 var parseSDdataToUIData = require('../protocol/parseSDdataToUIData');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db/db.sqlite3');
 
 exports.init = function (req, res) {
     res.render('analisys.ejs');
 }
+
+exports.queryItems = function(req, res) {
+    var keyword = req.query.keyword;
+    var line = "select * from items\
+                where ChnName LIKE '%" + keyword +"%' or EngName LIKE '%" + keyword + "%' or JapName LIKE '%" + keyword + "%' LIMIT 0, 30";
+    if (!isNaN(keyword))
+        line = "select * from items where id LIKE '" + keyword + "%' LIMIT 0, 20";
+    console.log(line);
+    db.serialize(function() {
+        db.prepare(line).all(function(err, rows) {
+        //    console.log(rows);
+       //     console.log(err);
+            if (rows) res.send(rows);
+            else res.send(null);
+        });
+    });        
+};
 
 exports.searchItemTrade = function(req, res) {
     var areaId = req.query.areaId;
@@ -12,28 +30,27 @@ exports.searchItemTrade = function(req, res) {
 
     var keyword = req.query.keyword;
     var uikindkey = req.query.uikindkey;
-
-    findItemInfo.exec(keyword, uikindkey, function(itemObj) {
-        if (itemObj == null && isNaN(keyword)) {
-            res.send({error : "没找到", id : keyword});
-        }
-        else {
-            var id = keyword;
-            if (itemObj) id = itemObj.id;
-            getItemTradeData.exec(id, areaId, groupId, function(tradeData) {
+    
+    var line = "select id from items where ChnName = ?";
+    console.log(keyword);
+    db.serialize(function () {
+        db.prepare(line).get(keyword, function(err, row) {
+            if (row) {
+                var id = row.Id;
+                console.log(row);
+                getItemTradeData.exec(id, areaId, groupId, function(tradeData) {
                 if (tradeData == null) {
                     res.send({error : "没找到", id : id});
                 }
                 else {
                     parseSDdataToUIData.exec(tradeData, function(uiData) {
-                        if (itemObj) {
-                            uiData.iconUrl = itemObj.iconUrl;
-                            uiData.hqIconUrl = itemObj.hqIconUrl;
-                        }
                         res.send({uiData : uiData});
                     })
                 }
             })
-        }
+            }
+            else
+                res.send({error : "没找到", keyword : keyword});
+        });
     });
 }
--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ var router = require('express').Router();
 var analisysController = require('../app/controller/analisysController');
 
 router.get('/searchItemTrade', analisysController.searchItemTrade);
+router.get('/queryItems', analisysController.queryItems);
 
 module.exports = router;
